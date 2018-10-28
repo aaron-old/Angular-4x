047445884c2df08ec75c93bc820578b2c1265b39
Aaron Young <mustarddevelopment@gmail.com>
2018-10-27 17:50:12 -0700

    adding some projects to make a full enterprise template w/ a business and data layer

---
+++ App.Core/Dto/Identity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Dto
+{
+    public class Identity
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
